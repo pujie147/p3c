c6606b350465951612616b26586491f6929806c4
pujie147 <pyg124@live.com>
2021-03-05 16:16:27 +0800

    添加   【强制】禁止在 POJO 类中，同时存在对应属性 xxx 的 isXxx()和  getXxx()方法。

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/oop/ClassPOJOGetAndIsRepeatedRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/oop/ClassPOJOGetAndIsRepeatedRule.java
@@ -1,53 +1,38 @@
-/*
- * Copyright 1999-2017 Alibaba Group.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.alibaba.p3c.pmd.lang.java.rule.oop;
 
 import com.alibaba.p3c.pmd.I18nResources;
-import com.alibaba.p3c.pmd.lang.java.rule.AbstractAliRule;
 import com.alibaba.p3c.pmd.lang.java.rule.AbstractPojoRule;
 import com.alibaba.p3c.pmd.lang.java.util.ViolationUtils;
-import com.alibaba.p3c.pmd.lang.java.util.namelist.NameListConfig;
-import net.sourceforge.pmd.RuleContext;
 import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceBody;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceBodyDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 
 import java.util.List;
-import java.util.regex.Pattern;
+
 
 /**
- * [Mandatory] Class names should be nouns in UpperCamelCase except domain models: DO, BO, DTO, VO, etc.
+ * [Mandatory] 禁止在 POJO 类中，同时存在对应属性 xxx 的 isXxx()和getXxx()方法
  *
- * @author changle.lq
- * @date 2017/04/16
+ * @author dell
+ * @date 2021/03/05
  */
 public class ClassPOJOGetAndIsRepeatedRule extends AbstractPojoRule {
 
     @Override
     public Object visit(ASTClassOrInterfaceBodyDeclaration node, Object data) {
-//        node.findChildNodesWithXPath("ClassOrInterfaceBody/ClassOrInterfaceBodyDeclaration/FieldDeclaration");
-//        node.findChildNodesWithXPath("ClassOrInterfaceBody/ClassOrInterfaceBodyDeclaration/MethodDeclaration");
-//        node.findChildNodesWithXPath("ClassOrInterfaceBody/ClassOrInterfaceBodyDeclaration/MethodDeclaration [@Public='true' and MethodDeclarator[ starts-with[@Image='get']] OR MethodDeclarator[ starts-with[@Image='is']] ");
-//
-//
-//        ViolationUtils.addViolationWithPrecisePosition(this, node, data,
-//            I18nResources.getMessage("java.naming.ClassNamingShouldBeCamelRule.violation.msg",
-//                node.getImage()));
-
+        try {
+            List<Node> fieldNames = node.findChildNodesWithXPath("//FieldDeclaration/VariableDeclarator/VariableDeclaratorId");
+            for(Node fieldName:fieldNames){
+                String captureName = fieldName.getImage().substring(0, 1).toUpperCase() + fieldName.getImage().substring(1);
+                List<Node> methods = node.findChildNodesWithXPath("//MethodDeclarator[@Image='is" + captureName + "'or @Image='get" + captureName + "']");
+                if(methods.size()>1){
+                    ViolationUtils.addViolationWithPrecisePosition(this, fieldName, data,
+                            I18nResources.getMessage("java.oop.ClassPOJOGetAndIsRepeatedRule.violation.msg",
+                                    fieldName.getImage()));
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return super.visit(node, data);
     }
 }
